eb16cf5e0d1f57551adc16984c5c7323d5ad6d79
Mitchell <13673076+Mishyy@users.noreply.github.com>
2022-11-04 19:00:29 +1000

    Add --test option for use in scripts.

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use std::{env, fs};
 
 use clap::{Parser, Subcommand};
 
-use crate::htb::{get_machine, get_machines, join_machine, leave_machine, own_machine};
+use crate::api::{get_machine, get_machines, join_machine, leave_machine, own_machine};
 use crate::machine::{Difficulty, Machine, OperatingSystem};
 
-mod htb;
+mod api;
 mod machine;
 
 const RES: &str = "\x1B[";
@@ -20,15 +20,14 @@ const HTB_MACHINE_PAGE: &str = "https://app.hackthebox.com/machines";
 #[derive(Parser)]
 #[command(name = "HTB")]
 #[command(author = "Mitchell <13673076+Mishyy@users.noreply.github.com>")]
-#[command(version)]
-#[command(about = "Command-line utility for interacting with the HackTheBox API.")]
-struct Args {
+#[command(about = "Interact with the HackTheBox API.", long_about = None)]
+struct Command {
     #[command(subcommand)]
-    command: Command,
+    command: Args,
 }
 
 #[derive(Subcommand)]
-enum Command {
+enum Args {
     #[command(about = "List playable machines")]
     List {
         #[arg(help = "Filter machines by operating system", long_help = None, value_enum)]
@@ -44,6 +43,8 @@ enum Command {
         eval: bool,
         #[arg(help = "Skip creating directory", long_help = None, long, default_value_t = false)]
         no_dir: bool,
+        #[arg(hide = true, long, default_value_t = false)]
+        test: bool,
     },
     #[command(about = "Join a live machine")]
     Join {
@@ -63,9 +64,9 @@ enum Command {
 }
 
 fn main() {
-    let args = Args::parse();
-    match &args.command {
-        Command::List {
+    let cli = Command::parse();
+    match &cli.command {
+        Args::List {
             os,
             difficulty: diff,
         } => match get_machines() {
@@ -88,60 +89,67 @@ fn main() {
                 exit(1);
             }
         },
-        Command::Info {
+        Args::Info {
             machine,
             eval,
             no_dir,
-        } => match *eval {
-            true => {
-                if !*no_dir {
-                    let home = Path::new(machine.home.as_str());
-                    if !home.is_dir() {
-                        if !home.parent().map(|p| p.exists()).unwrap_or(true) {
-                            eprintln!("Are you sure {} is a valid path?", machine.home);
-                            exit(2);
-                        } else if home.exists() {
-                            eprintln!("{} already exists but isn't a directory!", machine.home);
-                            exit(2);
-                        } else if let Err(error) = fs::create_dir(home) {
-                            eprintln!("create_dir: {}", error.to_string());
-                            exit(1);
+            test,
+        } => {
+            if *test {
+                exit(0);
+            }
+
+            match *eval {
+                true => {
+                    if !*no_dir {
+                        let home = Path::new(machine.home.as_str());
+                        if !home.is_dir() {
+                            if !home.parent().map(|p| p.exists()).unwrap_or(true) {
+                                eprintln!("Are you sure {} is a valid path?", machine.home);
+                                exit(2);
+                            } else if home.exists() {
+                                eprintln!("{} already exists but isn't a directory!", machine.home);
+                                exit(2);
+                            } else if let Err(error) = fs::create_dir(home) {
+                                eprintln!("create_dir: {}", error.to_string());
+                                exit(1);
+                            }
                         }
                     }
-                }
 
-                println!("export MACHINE_ID={}", machine.id);
-                println!("export MACHINE_NAME={}", machine.name);
-                println!("export MACHINE_IP={}", machine.ip);
-                println!(
-                    "export MACHINE_HOME=\"{}\"",
-                    machine.home.replace(
-                        &env::var("CS_OPT").expect("You must define CS_OPT!"),
-                        "$CS_OPT",
-                    )
-                );
+                    println!("export MACHINE_ID={}", machine.id);
+                    println!("export MACHINE_NAME={}", machine.name);
+                    println!("export MACHINE_IP={}", machine.ip);
+                    println!(
+                        "export MACHINE_HOME=\"{}\"",
+                        machine.home.replace(
+                            &env::var("CS_OPT").expect("You must define CS_OPT!"),
+                            "$CS_OPT"
+                        )
+                    );
+                }
+                false => {
+                    println!(
+                        "#{}> {RES}1m{}{RES}0m [{:#?} <> {:#?}] @ {}",
+                        machine.id, machine.name, machine.os, machine.difficulty, machine.ip
+                    );
+                    println!("{HTB_MACHINE_PAGE}/{}", machine.id);
+                }
             }
-            false => {
-                println!(
-                    "#{}> {RES}1m{}{RES}0m [{:#?} <> {:#?}] @ {}",
-                    machine.id, machine.name, machine.os, machine.difficulty, machine.ip
-                );
-                println!("{HTB_MACHINE_PAGE}/{}", machine.id);
-            }
-        },
-        Command::Join { machine } => {
+        }
+        Args::Join { machine } => {
             if let Err(message) = join_machine(&machine) {
-                eprintln!("spawn_machine: {message}");
+                eprintln!("join_machine: {message}");
                 exit(1);
             }
         }
-        Command::Leave => {
+        Args::Leave => {
             if let Err(message) = leave_machine() {
-                eprintln!("despawn_machine: {message}");
+                eprintln!("leave_machine: {message}");
                 exit(1);
             }
         }
-        Command::Submit {
+        Args::Submit {
             machine,
             flag,
             difficulty,
